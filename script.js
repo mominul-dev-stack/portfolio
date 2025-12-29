8dc8d32775b8cac0d5c9d5008323873ce7458384
mominul-dev-stack <mominul.project@gmail.com>
2025-12-29 11:22:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const text = "Frontend Developer";
+let i = 0;
+
+function type() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(type, 100);
+    }
+}
+type();
